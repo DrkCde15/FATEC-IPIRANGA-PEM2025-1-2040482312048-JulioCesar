abd75e05ff4a8e9cc2392f378047214e926400c8
JulioCesar <160616751+DrkCde15@users.noreply.github.com>
2025-06-07 09:32:57 -0300

    Update PEM-equipe4-projetoFinal.c

--- PEM_2025-1-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-4/PEM-equipe4-projetoFinal.c
+++ PEM_2025-1-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-4/PEM-equipe4-projetoFinal.c
@@ -15,23 +15,149 @@
 
 typedef struct {
     int id;
-    char nome[50];
-    char descricao[100];
+    char nome[100];
+    char descricao[200];
     float preco;
     int estoque;
 } Produto;
 
-// Prototipos das funcoes
-void inserirProduto(Produto *produtos, int *numProdutos);
-void listarProdutos(Produto *produtos, int numProdutos);
-void ordenarProdutosPorNome(Produto *produtos, int numProdutos);
-void simularCompra(Produto *produtos, int numProdutos);
-int buscarProdutoPorId(Produto *produtos, int numProdutos, int id);
-void limparBufferEntrada();
+void limparBufferEntrada() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
-int main() {
+void ordenarProdutosPorNome(Produto *produtos, int quantidade) {
+    for (int i = 0; i < quantidade - 1; i++) {
+        for (int j = 0; j < quantidade - i - 1; j++) {
+            if (strcmp(produtos[j].nome, produtos[j + 1].nome) > 0) {
+                Produto temp = produtos[j];
+                produtos[j] = produtos[j + 1];
+                produtos[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void listarProdutos(Produto *produtos, int quantidade) {
+    ordenarProdutosPorNome(produtos, quantidade);
+
+    printf("\n=== LISTA DE PRODUTOS ===\n");
+    for (int i = 0; i < quantidade; i++) {
+        printf("ID: %d\n", produtos[i].id);
+        printf("Nome: %s\n", produtos[i].nome);
+        printf("Descricao: %s\n", produtos[i].descricao);
+        printf("Preco: R$ %.2f\n", produtos[i].preco);
+        printf("Estoque: %d\n\n", produtos[i].estoque);
+    }
+}
+
+void inserirProduto(Produto *produtos, int *quantidade) {
+    if (*quantidade >= MAX_PRODUTOS) {
+        printf("Limite de produtos atingido.\n");
+        return;
+    }
+
+    Produto novo;
+
+    printf("Digite o ID do produto: ");
+    if (scanf("%d", &novo.id) != 1) {
+        printf("ID invalido.\n");
+        limparBufferEntrada();
+        return;
+    }
+
+    // Verificar se ID já existe
+    for (int i = 0; i < *quantidade; i++) {
+        if (produtos[i].id == novo.id) {
+            printf("Erro: Ja existe um produto com esse ID!\n");
+            return;
+        }
+    }
+
+    limparBufferEntrada(); // limpa buffer após scanf
+
+    printf("Nome do produto: ");
+    fgets(novo.nome, sizeof(novo.nome), stdin);
+    novo.nome[strcspn(novo.nome, "\n")] = '\0';
+
+    printf("Descricao do produto: ");
+    fgets(novo.descricao, sizeof(novo.descricao), stdin);
+    novo.descricao[strcspn(novo.descricao, "\n")] = '\0';
+
+    printf("Preco unitario (use ponto, ex: 12.90): ");
+    if (scanf("%f", &novo.preco) != 1) {
+        printf("Entrada invalida. Use ponto em vez de virgula.\n");
+        limparBufferEntrada();
+        return;
+    }
+
+    printf("Quantidade em estoque: ");
+    if (scanf("%d", &novo.estoque) != 1) {
+        printf("Quantidade invalida.\n");
+        limparBufferEntrada();
+        return;
+    }
+
+    produtos[*quantidade] = novo;
+    (*quantidade)++;
+    printf("Produto inserido com sucesso!\n");
+}
+
+void comprarProduto(Produto *produtos, int *quantidade) {
+    int id, quantidadeDesejada;
+    printf("Digite o ID do produto que deseja comprar: ");
+    if (scanf("%d", &id) != 1) {
+        printf("ID invalido.\n");
+        limparBufferEntrada();
+        return;
+    }
+
+    for (int i = 0; i < *quantidade; i++) {
+        if (produtos[i].id == id) {
+            printf("Produto encontrado: %s (Estoque: %d)\n", produtos[i].nome, produtos[i].estoque);
+
+            if (produtos[i].estoque == 0) {
+                printf("Este produto esta sem estoque no momento.\n");
+                return;
+            }
+
+            printf("Digite a quantidade desejada: ");
+            if (scanf("%d", &quantidadeDesejada) != 1) {
+                printf("Quantidade invalida.\n");
+                limparBufferEntrada();
+                return;
+            }
+
+            if (quantidadeDesejada <= 0) {
+                printf("Quantidade deve ser maior que zero.\n");
+                return;
+            }
+
+            if (quantidadeDesejada > produtos[i].estoque) {
+                printf("Estoque insuficiente.\n");
+                return;
+            }
+
+            produtos[i].estoque -= quantidadeDesejada;
+            float total = quantidadeDesejada * produtos[i].preco;
+
+            printf("Compra realizada! Total: R$ %.2f\n", total);
+            printf("Estoque atual do produto '%s': %d unidades.\n", produtos[i].nome, produtos[i].estoque);
+
+            if (produtos[i].estoque == 0) {
+                printf("Aviso: O produto '%s' agora esta sem estoque.\n", produtos[i].nome);
+            }
+
+            return;
+        }
+    }
+
+    printf("Produto inexistente.\n");
+}
+
+void menu() {
     Produto produtos[MAX_PRODUTOS];
-    int numProdutos = 0;
+    int quantidade = 0;
     int opcao;
 
     do {
@@ -41,141 +167,33 @@ int main() {
         printf("3. Comprar produto\n");
         printf("0. Sair\n");
         printf("Opcao: ");
-        scanf("%d", &opcao);
-        limparBufferEntrada();
+        if (scanf("%d", &opcao) != 1) {
+            printf("Opcao invalida.\n");
+            limparBufferEntrada();
+            continue;
+        }
 
         switch (opcao) {
             case 1:
-                inserirProduto(produtos, &numProdutos);
+                inserirProduto(produtos, &quantidade);
                 break;
             case 2:
-                listarProdutos(produtos, numProdutos);
+                listarProdutos(produtos, quantidade);
                 break;
             case 3:
-                simularCompra(produtos, numProdutos);
+                comprarProduto(produtos, &quantidade);
                 break;
             case 0:
                 printf("Saindo do programa...\n");
                 break;
             default:
-                printf("Opcao invalida.\n");
+                printf("Opcao invalida. Tente novamente.\n");
         }
 
     } while (opcao != 0);
+}
 
+int main() {
+    menu();
     return 0;
-}
-
-void inserirProduto(Produto *produtos, int *numProdutos) {
-    if (*numProdutos >= MAX_PRODUTOS) {
-        printf("Limite de produtos atingido.\n");
-        return;
-    }
-
-    Produto *p = &produtos[*numProdutos];
-
-    printf("\nDigite o ID do produto: ");
-    scanf("%d", &p->id);
-    limparBufferEntrada();
-
-    printf("Nome do produto: ");
-    fgets(p->nome, sizeof(p->nome), stdin);
-    p->nome[strcspn(p->nome, "\n")] = '\0';
-
-    printf("Descricao do produto: ");
-    fgets(p->descricao, sizeof(p->descricao), stdin);
-    p->descricao[strcspn(p->descricao, "\n")] = '\0';
-
-    printf("Preco unitario: ");
-    scanf("%f", &p->preco);
-
-    printf("Quantidade em estoque: ");
-    scanf("%d", &p->estoque);
-    limparBufferEntrada();
-
-    (*numProdutos)++;
-    printf("Produto inserido com sucesso!\n");
-}
-
-void listarProdutos(Produto *produtos, int numProdutos) {
-    if (numProdutos == 0) {
-        printf("\nNenhum produto cadastrado.\n");
-        return;
-    }
-
-    ordenarProdutosPorNome(produtos, numProdutos);
-
-    printf("\n=== LISTA DE PRODUTOS ===\n");
-    for (int i = 0; i < numProdutos; i++) {
-        printf("ID: %d\n", produtos[i].id);
-        printf("Nome: %s\n", produtos[i].nome);
-        printf("Descricao: %s\n", produtos[i].descricao);
-        printf("Preco: R$ %.2f\n", produtos[i].preco);
-        printf("Estoque: %d\n\n", produtos[i].estoque);
-    }
-}
-
-void ordenarProdutosPorNome(Produto *produtos, int numProdutos) {
-    Produto temp;
-    for (int i = 0; i < numProdutos - 1; i++) {
-        for (int j = 0; j < numProdutos - i - 1; j++) {
-            if (strcmp(produtos[j].nome, produtos[j + 1].nome) > 0) {
-                temp = produtos[j];
-                produtos[j] = produtos[j + 1];
-                produtos[j + 1] = temp;
-            }
-        }
-    }
-}
-
-void simularCompra(Produto *produtos, int numProdutos) {
-    if (numProdutos == 0) {
-        printf("\nNenhum produto disponivel para compra.\n");
-        return;
-    }
-
-    int id, quantidade;
-    printf("\nDigite o ID do produto que deseja comprar: ");
-    scanf("%d", &id);
-
-    int indice = buscarProdutoPorId(produtos, numProdutos, id);
-    if (indice == -1) {
-        printf("Produto inexistente.\n");
-        return;
-    }
-
-    Produto *p = &produtos[indice];
-
-    printf("Produto encontrado: %s (Estoque: %d)\n", p->nome, p->estoque);
-    printf("Digite a quantidade desejada: ");
-    scanf("%d", &quantidade);
-
-    if (quantidade <= 0) {
-        printf("Quantidade invalida.\n");
-        return;
-    }
-
-    if (quantidade > p->estoque) {
-        printf("Estoque insuficiente.\n");
-    } else {
-        float total = p->preco * quantidade;
-        p->estoque -= quantidade;
-        printf("Compra realizada com sucesso!\n");
-        printf("Total da compra: R$ %.2f\n", total);
-    }
-}
-
-int buscarProdutoPorId(Produto *produtos, int numProdutos, int id) {
-    for (int i = 0; i < numProdutos; i++) {
-        if (produtos[i].id == id) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-// Funcao para limpar buffer de entrada (stdin)
-void limparBufferEntrada() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
 }
